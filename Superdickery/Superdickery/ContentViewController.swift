7f65f38cf0c2c4481a4a9a3689189fc7c64bd0a7
mikemiksch <mikemiksch@gmail.com>
2017-08-21 14:18:17 -0700

    Need to solve race condition where array of UIImageViews isn't populated before view controller attempts to assign them constraints.

--- Superdickery/Superdickery/ContentViewController.swift
+++ Superdickery/Superdickery/ContentViewController.swift
@@ -14,16 +14,26 @@ import SwiftSoup
 class ContentViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var titleText = String()
     var imageElements = [Element]()
     var textContent = [String]()
+    var contentHeight = 0
+    var imageViews = [UIImageView]()
+    var labels = [UILabel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         parseHTML(html: fetchPage())
         titleLabel.text = titleText
-        renderImages()
+        scrollView.backgroundColor = UIColor.white
+        scrollView.contentSize.width = self.view.bounds.width
+        scrollView.contentSize.height = 1000
+        generateImageViews() { result in
+            setImageViewConstraints()
+        }
+//        setImageViewConstraints()
         
     }
     
@@ -52,26 +62,37 @@ class ContentViewController: UIViewController {
         }
     }
     
-    func renderImages() {
+    func generateImageViews(completion: () -> Void ) {
         for each in imageElements {
             let width = Double(self.view.bounds.size.width * 0.9)
             let height = try! Double(each.attr("height"))!
-//            let imageRatio = height/width
             let src = try! each.attr("src")
             let url = URL(string:src)!
             DispatchQueue.global(qos: .userInitiated).async {
                 let imageData = NSData(contentsOf: url)!
                 let imageView = UIImageView(frame: CGRect(x: 0, y:0, width: width, height:height))
                 imageView.center = self.view.center
-                
                 DispatchQueue.main.async {
                     let image = UIImage(data: imageData as Data)
                     imageView.image = image
                     imageView.contentMode = UIViewContentMode.scaleAspectFit
-                    self.view.addSubview(imageView)
+                    self.imageViews.append(imageView)
+                    print("here are image views when it should be appended: \(self.imageViews)")
+//                    self.view.addSubview(imageView)
                 }
             }
         }
+
     }
 
+    func setImageViewConstraints () {
+        print("Here are image views when ImageView Constraints is called \(self.imageViews)")
+        if !self.imageViews.isEmpty {
+            print("Hi there!")
+            scrollView.addSubview(imageViews[0])
+            let topConstraint = NSLayoutConstraint(item: imageViews[0], attribute: .top, relatedBy: .equal, toItem: self.titleLabel, attribute: .bottom, multiplier: 1.0, constant: 20.0)
+            scrollView.addConstraint(topConstraint)
+        }
+    }
 }
+    
