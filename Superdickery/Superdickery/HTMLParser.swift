582d7d66e67ad81eef19d7f2fb0fa19a72007f43
mikemiksch <mikemiksch@gmail.com>
2017-11-06 11:43:10 -0800

    Trying to chase down bug where code that shouldn't be doing anything breaks everything when commented out.

--- Superdickery/Superdickery/HTMLParser.swift
+++ Superdickery/Superdickery/HTMLParser.swift
@@ -68,45 +68,45 @@ class HTMLParser {
         return html
     }
     
-    func parseHTML(html: String) {
-        imageElements.removeAll()
-        labelTexts.removeAll()
-        let html = html
-        let doc : Document = try! SwiftSoup.parse(html)
-        let qfat = Array(try! doc.select("article").select(".category-qfat"))
-        let blog = Array(try! doc.select("article").select(".category-blog"))
-        if qfat.isEmpty && blog.isEmpty {
-            title = try! doc.select("h1").text()
-            if title == "Did You Know?" {
-                print("Did You Know post")
-                parseHTML(html: fetchPage())
-            } else {
-                let linkRels = try! doc.select("head").select("link").array()
-                let filteredLinks = linkRels.filter { link in
-                    try! link.attr("title").contains("Comments Feed")
-                }
-                shareURL = try! filteredLinks[1].attr("href")
-                shareURL = shareURL.substring(to: shareURL.index(shareURL.endIndex, offsetBy: -5))
-                let images = try! doc.select(".aligncenter").array()
-                let pTags = try! doc.select(".no-bottom").select("p").array()
-                for each in images {
-                    imageElements.append(each)
-                }
-                for each in pTags {
-                    let text = try! each.text()
-                    if text != "" && text != "Source" && text != "Unsourced" && !text.contains("©") {
-                        labelTexts.append(text)
-                    }
-                }
-                print(title)
-                if imageElements.isEmpty {
-                    print("imageElements is empty")
-                    parseHTML(html: fetchPage())
-                }
-            }
-        } else {
-            print("QFAT or blog post")
-            parseHTML(html: fetchPage())
-        }
-    }
+//    func parseHTML(html: String) {
+//        imageElements.removeAll()
+//        labelTexts.removeAll()
+//        let html = html
+//        let doc : Document = try! SwiftSoup.parse(html)
+//        let qfat = Array(try! doc.select("article").select(".category-qfat"))
+//        let blog = Array(try! doc.select("article").select(".category-blog"))
+//        if qfat.isEmpty && blog.isEmpty {
+//            title = try! doc.select("h1").text()
+//            if title == "Did You Know?" {
+//                print("Did You Know post")
+//                parseHTML(html: fetchPage())
+//            } else {
+//                let linkRels = try! doc.select("head").select("link").array()
+//                let filteredLinks = linkRels.filter { link in
+//                    try! link.attr("title").contains("Comments Feed")
+//                }
+//                shareURL = try! filteredLinks[1].attr("href")
+//                shareURL = shareURL.substring(to: shareURL.index(shareURL.endIndex, offsetBy: -5))
+//                let images = try! doc.select(".aligncenter").array()
+//                let pTags = try! doc.select(".no-bottom").select("p").array()
+//                for each in images {
+//                    imageElements.append(each)
+//                }
+//                for each in pTags {
+//                    let text = try! each.text()
+//                    if text != "" && text != "Source" && text != "Unsourced" && !text.contains("©") {
+//                        labelTexts.append(text)
+//                    }
+//                }
+//                print(title)
+//                if imageElements.isEmpty {
+//                    print("imageElements is empty")
+//                    parseHTML(html: fetchPage())
+//                }
+//            }
+//        } else {
+//            print("QFAT or blog post")
+//            parseHTML(html: fetchPage())
+//        }
+//    }
 }
